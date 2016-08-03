3a4c05aa94efca1e5fa3d730d0bffae321113c3b
yelyn <yelyn1249@gmail.com>
2016-08-03 23:08:42 +0900

    test Commit

---
+++ OXDescription.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OXDescription : MonoBehaviour
+{
+    public void LoadNextLevel() {
+        StartCoroutine(LevelLoad());
+      
+        SceneManager.LoadScene("Main");
+    }
+   IEnumerator LevelLoad() {
+        yield return new WaitForSeconds(10f);
+        
+    }
+ 
+} 
